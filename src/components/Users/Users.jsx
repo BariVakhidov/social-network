35ae441613fca91c079002b3b80488dcda90e5fd
Bary <bari.vaxidov@mail.ru>
2021-01-14 12:47:44 +0300

    fix double users data

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,21 +1,21 @@
 import React from 'react';
 import s from "./Users.module.css"
 import * as axios from "axios";
+import userPhoto from '../../assets/images/pepe.png'
 
 const Users = (props) => {
-
-    if (props.users.length === 0) {
-        axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
-            props.setUsers(response.data.items);
-        });
+    let getUsers = () => {
+            axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
+                props.setUsers(response.data.items);
+            });
     }
     return (
         <div>
             {props.users.map(u =>
-                    <div key={u.id}>
+                <div key={u.id}>
                 <span>
                     <div>
-                        <img className={s.userPhoto} src={u.photoUrl} alt=""/>
+                        <img className={s.userPhoto} src={u.photos.small != null ? u.photos.small : userPhoto} alt=""/>
                     </div>
                     <div>
                         {u.isFollow ? <button onClick={() => {
@@ -25,16 +25,16 @@ const Users = (props) => {
                         }}>Follow</button>}
                     </div>
                 </span>
-                        <span>
+                    <span>
                     <span>
                         <div>{u.name}</div>
                         <div>{u.status}</div>
                     </span>
                     <span>
-
                     </span>
                 </span>
-                    </div>)}
+                </div>)}
+            <button onClick={getUsers}>Get users</button>
         </div>
     );
 };
